c5a303948e263835ed1ab0cf9e70f59ac5da2e58
mizotii <mztiiiii0@gmail.com>
2026-03-31 17:50:47 -0700

    clean up stuff, start converting raw data to objects, add comments

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
+from pprint import pp
 import re
 from collections import defaultdict
-from chart import Chart
+from chart import BGMEvent, BPMChange, Chart, Note, MeasureLine, StopEvent
 
 HEADER_PATTERN = re.compile('^#([A-Za-z][A-Za-z0-9]*)[ \t]+(.+)$')
 DATA_PATTERN = re.compile(r'^#(\d{3})([0-9A-Za-z]{2}):(.*)$')
@@ -24,11 +25,18 @@ def parse_bms(filepath):
 
     # for calculating timing / storing data lines
     measure_count = 0
-    total_beats = 0.0
 
-    raw_data = defaultdict(list)
+    raw_data = []
     measure_lengths = {}
+    measure_to_absolute_beat = {}
 
+    notes = []
+    bpm_changes = []
+    measure_lines = []
+    bgm_events = []
+    stop_events = []
+
+    # initial pass for headers and storing data
     with open(filepath, 'rb') as f:
         lines = f.readlines()
         for line in lines:
@@ -37,6 +45,7 @@ def parse_bms(filepath):
             except ValueError:
                 decoded_line = line.decode('latin-1')
             
+            # store headers
             m = HEADER_PATTERN.search(decoded_line)
             if m:
                 k = m.group(1).upper()
@@ -90,7 +99,8 @@ def parse_bms(filepath):
                             pass
 
                 continue
-
+            
+            # or store data
             m = DATA_PATTERN.search(decoded_line)
             if m:
                 measure, channel, data = m.groups()
@@ -106,22 +116,28 @@ def parse_bms(filepath):
                         except:
                             pass
                     case _:
-                        raw_data[(measure, channel)].append(data)
-
+                        raw_data.append(measure, channel, data)
                 measure_count = max(measure, measure_count)
 
                 continue
-    
-    # beat to time
-    # import pprint
-    # pprint.pp(raw_data)
+
+    # get the absolute beat position of the start of every measure
+    total_beats = 0.0
+
     for i in range(1, measure_count + 1):
         possible_measure_length = measure_lengths.get(i)
         measure_length = possible_measure_length if possible_measure_length is not None else 1.0
         total_beats += measure_length * 4
+        measure_to_absolute_beat[i] = total_beats
 
-    print(total_beats)
-
+    # convert raw data to objects
+    for r in raw_data:
+        # tuple: measure, channel, data
+        match r[1]:
+            case '01':
+                bgm_events.append(
+                    BGMEvent(measure_to_absolute_beat[r[0]], )
+                )
 
 if __name__ == '__main__':
     parse_bms('./ceu/7keys_white.bms')
